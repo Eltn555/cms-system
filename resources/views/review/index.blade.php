737fb143a1d60b1b59401b68ad456b7b2fbcb2c9
Eltn555 <eltnkhan2555@gmail.com>
2023-12-26 16:36:37 +0500

    Review some changes

--- resources/views/review/index.blade.php
+++ resources/views/review/index.blade.php
@@ -208,7 +208,6 @@
                 ajax('', '', id, 'Delete');
             });
             function ajax(field, newValue, reviewID, method) {
-                // alert('field:'+field+"\n value:"+newValue+"\n ID:"+reviewID);
                 $.ajax({
                     url: "reviews/" + reviewID, // Replace with your route for updating the category
                     method: method,
@@ -229,109 +228,5 @@
                 });
             }
         });
-
-        // ************************ Drag and drop ***************** //
-        let dropArea = document.getElementById("drop-area")
-
-// Prevent default drag behaviors
-        ;['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
-            dropArea.addEventListener(eventName, preventDefaults, false)
-            document.body.addEventListener(eventName, preventDefaults, false)
-        })
-
-// Highlight drop area when item is dragged over it
-        ;['dragenter', 'dragover'].forEach(eventName => {
-            dropArea.addEventListener(eventName, highlight, false)
-        })
-
-        ;['dragleave', 'drop'].forEach(eventName => {
-            dropArea.addEventListener(eventName, unhighlight, false)
-        })
-
-        // Handle dropped files
-        dropArea.addEventListener('drop', handleDrop, false)
-
-        function preventDefaults (e) {
-            e.preventDefault()
-            e.stopPropagation()
-        }
-
-        function highlight(e) {
-            dropArea.classList.add('highlight')
-        }
-
-        function unhighlight(e) {
-            dropArea.classList.remove('active')
-        }
-
-        function handleDrop(e) {
-            var dt = e.dataTransfer
-            var files = dt.files
-
-            handleFiles(files)
-        }
-
-        let uploadProgress = []
-        let progressBar = document.getElementById('progress-bar')
-
-        function initializeProgress(numFiles) {
-            progressBar.value = 0
-            uploadProgress = []
-
-            for(let i = numFiles; i > 0; i--) {
-                uploadProgress.push(0)
-            }
-        }
-
-        function updateProgress(fileNumber, percent) {
-            uploadProgress[fileNumber] = percent
-            let total = uploadProgress.reduce((tot, curr) => tot + curr, 0) / uploadProgress.length
-            progressBar.value = total
-        }
-
-        function handleFiles(files) {
-            files = [...files]
-            initializeProgress(files.length)
-            files.forEach(uploadFile)
-            files.forEach(previewFile)
-        }
-
-        function previewFile(file) {
-            let reader = new FileReader()
-            reader.readAsDataURL(file)
-            reader.onloadend = function() {
-                let img = document.createElement('img')
-                img.src = reader.result
-                document.getElementById('gallery').appendChild(img)
-            }
-        }
-
-        function uploadFile(file, i) {
-            var url = 'upload'
-            var xhr = new XMLHttpRequest();
-            var formData = new FormData();
-            xhr.open('POST', url, true);
-            xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-
-            // Include CSRF token, required by Laravel for POST requests
-            // var csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-            // xhr.setRequestHeader('X-CSRF-TOKEN', csrfToken);
-
-
-            xhr.addEventListener('readystatechange', function(e) {
-                if (xhr.readyState == 4 && xhr.status == 200) {
-                    updateProgress(i, 100) // <- Add this
-                    // var response = JSON.parse(xhr.responseText);
-                    // alert(response);
-                }
-                else if (xhr.readyState == 4 && xhr.status != 200) {
-                    // Error. Inform the user
-                }
-            })
-
-            // formData.append('upload_preset', 'ujpu6gyk')
-            formData.append('file', file)
-            xhr.send(formData)
-        }
     </script>
 @endsection
