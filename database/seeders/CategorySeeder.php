8fb2795425f31f2d05e8513cdbb56adf6aca41e0
Eltn555 <eltnkhan2555@gmail.com>
2024-01-01 19:48:56 +0500

    Seed

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $categories = [
+            'Люстра',
+            'Бра',
+            'Торшер',
+            'Одинарный люстра',
+            'Подвесной люстра',
+            'Галоген',
+            'Магнитная система'
+        ];
+
+        foreach ($categories as $category) {
+            DB::table('categories')->insert([
+                'parent_category_id' => null, // Assuming top-level categories
+                'order_id' => rand(1, 10), // Random order_id for example
+                'title' => $category,
+                'description' => Str::random(50), // Random string for description
+                'image' => null, // Assuming no image
+                'seo_title' => 'SEO Title for ' . $category,
+                'seo_description' => 'SEO Description for ' . $category,
+                'is_active' => true,
+                'created_at' => now(),
+                'updated_at' => now()
+            ]);
+        }
+    }
+}
