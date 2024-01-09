8ada92a030e1ed68f9d5063e770c1812336269c4
Eltn555 <eltnkhan2555@gmail.com>
2024-01-09 23:50:24 +0500

    Changes by ramziddinabdumominov

--- resources/views/front/category/index.blade.php
+++ resources/views/front/category/index.blade.php
@@ -48,31 +48,31 @@
 </div>
 @section('scripts')
 
-   <script type="text/javascript">
+    <script type="text/javascript">
 
-       // Changing Title
+        // Changing Title
 
-       function changeTitle(id) {
+        function changeTitle(id) {
             console.log(id)
-           $.ajax({
-               url: '{{ route('front.category.search') }}',
-               method: 'GET',
-               encode: true,
-               data: {
-                   id: id,
-               },
-               success: function (response) {
-                   $('#main-title').html(response.title)
-                   $('#product-list').html(response.productsList)
-                   console.log(response.productsList);
-               },
-               error: function (error) {
-                   console.error('Update failed:', error);
-               }
-           });
+            $.ajax({
+                url: '{{ route('front.category.search') }}',
+                method: 'GET',
+                encode: true,
+                data: {
+                    id: id,
+                },
+                success: function (response) {
+                    $('#main-title').html(response.title)
+                    $('#product-list').html(response.productsList)
+                    console.log(response.productsList);
+                },
+                error: function (error) {
+                    console.error('Update failed:', error);
+                }
+            });
 
-       }
+        }
 
-   </script>
+    </script>
 
 @endsection
