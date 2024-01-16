0647ae975cf6b953cc5ae397a913a5a1df9990c1
mamadjanovakbar@gmail.com <AkbarGitHub8522>
2024-01-16 20:51:23 +0600

    Categories in progress

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -968,36 +968,14 @@
         <div class="container">
             <div class="brand-logo-active swiper-container">
                 <div class="swiper-wrapper">
-                    <div class="swiper-slide">
-                        <div class="single-brand-logo" data-aos="fade-up" data-aos-delay="200">
-                            <a href="#"><img src="assets/images/brand-logo/brand-logo-1.png" alt=""></a>
+
+                    @foreach($partners as $key =>$partner)
+                        <div class="swiper-slide">
+                            <div class="single-brand-logo" data-aos="fade-up" data-aos-delay="{{ ($key + 1) * 200 }}">
+                                <a href="#"><img src="{{ asset('storage/' . $partner->image) }}" alt=""></a>
+                            </div>
                         </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="single-brand-logo" data-aos="fade-up" data-aos-delay="400">
-                            <a href="#"><img src="assets/images/brand-logo/brand-logo-2.png" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="single-brand-logo" data-aos="fade-up" data-aos-delay="600">
-                            <a href="#"><img src="assets/images/brand-logo/brand-logo-3.png" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="single-brand-logo" data-aos="fade-up" data-aos-delay="800">
-                            <a href="#"><img src="assets/images/brand-logo/brand-logo-4.png" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="single-brand-logo" data-aos="fade-up" data-aos-delay="1000">
-                            <a href="#"><img src="assets/images/brand-logo/brand-logo-5.png" alt=""></a>
-                        </div>
-                    </div>
-                    <div class="swiper-slide">
-                        <div class="single-brand-logo" data-aos="fade-up" data-aos-delay="1200">
-                            <a href="#"><img src="assets/images/brand-logo/brand-logo-1.png" alt=""></a>
-                        </div>
-                    </div>
+                    @endforeach
                 </div>
             </div>
         </div>
