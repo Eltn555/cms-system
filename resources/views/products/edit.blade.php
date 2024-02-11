7f0ce6e401c622790f3e1f6497c42d1083d18de9
Eltn555 <eltnkhan2555@gmail.com>
2024-02-12 01:06:09 +0500

    Product update fixed

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -120,18 +120,25 @@
 @section('content')
 
     <h2 class="intro-y text-lg font-medium my-6">Create New Product</h2>
-    <form action="{{ route('admin.products.update',$products->id) }}" method="POST" enctype="multipart/form-data">
-        @csrf
-        @method('PUT')
-        <div class="intro-y box m-auto w-2/3 py-5 px-5 grid col-span-12">
+        <div class="intro-y box m-auto py-5 px-5 grid grid-cols-12 w-100">
             <div class="col-span-12">
                 <label class="form-label">Upload Image</label>
                 <div id="dropBox" class="border-2 border-dashed dark:border-darkmode-400 rounded-md p-2">
                     <div class="flex flex-wrap px-4">
                         <div id="imgs" class="flex">
                             <!-- rasmla shettan chqadi -->
+                            @foreach($products->images as $image)
+                                <div class="w-24 h-24 relative image-fit mb-5 mr-5 cursor-pointer zoom-in">
+                                    <img class="rounded-md" src="{{asset('storage/'.$image->image)}}">
+                                    <button data-action="{{$image->id}}" class="deleteImage w-5 h-5 flex items-center justify-center absolute text-center rounded-full text-white bg-danger right-0 top-0 -mr-2 -mt-2">
+                                        ✘
+                                    </button>
+                                </div>
+                            @endforeach
                         </div>
-                        <div class="avatar-wrapper w-24 h-24 image-fit zoom-in">
+                        <form id="" enctype="multipart/form-data">
+                            @csrf
+                            <div class="avatar-wrapper w-24 h-24 image-fit zoom-in">
                             <img id="" class="img_category profile-pic w-24 h-24" src="{{asset('add.png')}}"/>
                             <div class="upload-button flex items-center justify-center rounded">
                                 <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
@@ -156,109 +163,113 @@
                                 </div>
                             </div>
                         </div>
+                        </form>
                     </div>
                 </div>
             </div>
-            <div class="col-span-6 mx-2 sm:col-span-5 mt-3">
-                <label for="Title" class="form-label">Title</label>
-                <input id="Title" name="title" required type="text" class="form-control" placeholder="Table Lamp"
-                       value="{{ $products->title }}">
-            </div>
-            <div class="col-span-6 mx-2 sm:col-span-5 mt-3">
-                <label for="Price" class="form-label"><b class="text-danger">* </b>Price</label>
-                <input id="Price" required name="price" type="number" class="form-control"
-                       placeholder="Enter a price..." value="{{$products->price}}">
-            </div>
-            <div class="col-span-6 mx-2 sm:col-span-5 mt-3">
-                <label for="Discount-price" class="form-label">Discount Price</label>
-                <input id="Discount-price" name="discount_price" type="number" class="form-control"
-                       placeholder="Enter a discount price... (optional)" value="{{$products->discount_price}}">
-            </div>
-            <div class="col-span-4 mx-2 sm:col-span-5 mt-3">
-                <label for="category" class="form-label"><b class="text-danger">* </b>Category</label>
-                <select id="category" required name="category_id" class="form-select">
-                    @foreach($categories as $category)
-                        <option value="{{ $category->id }}">
-                            {{ $category->title }}
-                            @endforeach
-                        </option>
-                </select>
-            </div>
-            <div class="col-span-2 mx-3 sm:col-span-2 mt-3">
-                <label for="visible" class="form-label">Visible</label>
-                <div class="form-check form-switch p-0">
-                    <input id="visible" class="form-check-input"
-                           name="status" type="checkbox" value="{{$products->status}}" checked>
+            <form action="{{ route('admin.products.update',$products->id) }}" method="POST" class="col-span-12 grid grid-cols-12" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="col-span-6 mx-2 sm:col-span-6 mt-3">
+                    <label for="Title" class="form-label">Title</label>
+                    <input id="Title" name="title" required type="text" class="form-control" placeholder="Table Lamp"
+                           value="{{ $products->title }}">
                 </div>
-            </div>
+                <div class="col-span-6 mx-2 sm:col-span-5 mt-3">
+                    <label for="Price" class="form-label"><b class="text-danger">* </b>Price</label>
+                    <input id="Price" required name="price" type="number" class="form-control"
+                           placeholder="Enter a price..." value="{{$products->price}}">
+                </div>
+                <div class="col-span-4 mx-2 sm:col-span-5 mt-3">
+                    <label for="category" class="form-label"><b class="text-danger">* </b>Category</label>
+                    <select id="category" required name="category_id" class="form-select">
+                        @foreach($categories as $category)
+                            <option value="{{ $category->id }}">
+                                {{ $category->title }}
+                                @endforeach
+                            </option>
+                    </select>
+                </div>
+                <div class="col-span-2 mx-3 sm:col-span-2 mt-3">
+                    <label for="visible" class="form-label">Visible</label>
+                    <div class="form-check form-switch p-0">
+                        <input id="visible" class="form-check-input"
+                               name="status" type="checkbox" value="{{$products->status}}" checked>
+                    </div>
+                </div>
+                <div class="col-span-6 mx-2 sm:col-span-5 mt-3">
+                    <label for="Discount-price" class="form-label">Discount Price</label>
+                    <input id="Discount-price" name="discount_price" type="number" class="form-control"
+                           placeholder="Enter a discount price... (optional)" value="{{$products->discount_price}}">
+                </div>
 
-            <div class="col-span-12 sm:col-span-12 mt-3">
-                <label for="short-description" class="form-label">Description</label>
-                <textarea id="short-description" class="form-control h-[110px]" name="short_description"
-                          placeholder="Short Description">{{$products->short_description}}</textarea>
-            </div>
-            <div class="col-span-12 sm:col-span-12 mt-3">
-                <label for="modal-form-3" class="form-label">Long Description</label>
-                <textarea id="modal-form-3" class="tinyeditor form-control h-[110px]" name="long_description"
-                          placeholder="Long Description">
-                            {{$products->long_description}}
-                </textarea>
-            </div>
+                <div class="col-span-12 sm:col-span-12 mt-3">
+                    <label for="short-description" class="form-label">Description</label>
+                    <textarea id="short-description" class="form-control h-[110px]" name="short_description"
+                              placeholder="Short Description">{{$products->short_description}}</textarea>
+                </div>
+                <div class="col-span-12 sm:col-span-12 mt-3">
+                    <label for="modal-form-3" class="form-label">Long Description</label>
+                    <textarea id="modal-form-3" class="tinyeditor form-control h-[110px]" name="long_description"
+                              placeholder="Long Description">
+                                {{$products->long_description}}
+                    </textarea>
+                </div>
 
-            <div class="col-span-12 sm:col-span-12 mt-3">
-                <label for="additional" class="form-label">Additional</label>
-                <textarea id="additional" class="tinyeditor form-control h-[110px]" name="additional"
-                          placeholder="Additional">{!! $products->additional !!}</textarea>
-            </div>
+                <div class="col-span-12 sm:col-span-12 mt-3">
+                    <label for="additional" class="form-label">Additional</label>
+                    <textarea id="additional" class="tinyeditor form-control h-[110px]" name="additional"
+                              placeholder="Additional">{!! $products->additional !!}</textarea>
+                </div>
 
-            <div class="col-span-6 mx-2 sm:col-span-6 mt-3">
-                <label for="post-form-3-tomselected" class="form-label" id="post-form-3-ts-label">Similar
-                    products</label>
-                <select data-placeholder="Select tags" class="tom-select w-full tomselected"
-                        id="post-form-3" name="tags[]" required
-                        multiple="multiple" tabindex="-1" hidden="hidden">
-                    @foreach($tags as $tag)
-                        @if($products->tags->contains($tag->id))
-                            <option value="{{ $tag->title }}" selected>{{ $tag->title }}</option>
-                        @else
-                            <option value="{{ $tag->title }}">{{ $tag->title }}</option>
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-span-6 mx-2 sm:col-span-6 mt-3">
-                <label for="post-form-3-tomselected" class="form-label" id="post-form-3-ts-label">SEO</label>
-                <input id="modal-form-1" name="seo_title" type="text" class="form-control"
-                       placeholder="SEO Title" value="{{$products->seo_title}}">
-            </div>
-            <div class="col-span-6 mx-2 sm:col-span-6 mt-3 h-[110px]">
-                <label for="post-form-3-tomselected" class="form-label" id="post-form-3-ts-label">Additional
-                    products</label>
-                <select data-placeholder="Select categories" class="tom-select w-full tomselected"
-                        id="post-form-3" name="additional_products[]"
-                        multiple="multiple" required tabindex="-1" hidden="hidden">
-                    @foreach($tags as $tag)
-                        @if($products->additional_tags->contains($tag->id))
-                            <option value="{{ $tag->title }}" selected>{{ $tag->title }}</option>
-                        @else
-                            <option value="{{ $tag->title }}">{{ $tag->title }}</option>
-                        @endif
-                    @endforeach
-                </select>
-            </div>
-            <div class="col-span-6 mx-2 sm:col-span-6 mt-3">
-                <label for="modal-form-4" class="form-label">SEO Description</label>
-                <textarea id="modal-form-4" class="form-control h-[215px]" name="seo_description"
-                          placeholder="SEO Description" rows="3">{{$products->seo_description}}</textarea>
-            </div>
+                <div class="col-span-6 mx-2 sm:col-span-6 mt-3">
+                    <label for="post-form-3-tomselected" class="form-label" id="post-form-3-ts-label">Similar
+                        products</label>
+                    <select data-placeholder="Select tags" class="tom-select w-full tomselected"
+                            id="post-form-3" name="tags[]" required
+                            multiple="multiple" tabindex="-1" hidden="hidden">
+                        @foreach($tags as $tag)
+                            @if($products->tags->contains($tag->id))
+                                <option value="{{ $tag->title }}" selected>{{ $tag->title }}</option>
+                            @else
+                                <option value="{{ $tag->title }}">{{ $tag->title }}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-span-6 mx-2 sm:col-span-6 mt-3">
+                    <label for="post-form-3-tomselected" class="form-label" id="post-form-3-ts-label">SEO</label>
+                    <input id="modal-form-1" name="seo_title" type="text" class="form-control"
+                           placeholder="SEO Title" value="{{$products->seo_title}}">
+                </div>
+                <div class="col-span-6 mx-2 sm:col-span-6 mt-3 h-[110px]">
+                    <label for="post-form-3-tomselected" class="form-label" id="post-form-3-ts-label">Additional
+                        products</label>
+                    <select data-placeholder="Select categories" class="tom-select w-full tomselected"
+                            id="post-form-3" name="additional_products[]"
+                            multiple="multiple" required tabindex="-1" hidden="hidden">
+                        @foreach($tags as $tag)
+                            @if($products->additional_tags->contains($tag->id))
+                                <option value="{{ $tag->title }}" selected>{{ $tag->title }}</option>
+                            @else
+                                <option value="{{ $tag->title }}">{{ $tag->title }}</option>
+                            @endif
+                        @endforeach
+                    </select>
+                </div>
+                <div class="col-span-6 mx-2 sm:col-span-6 mt-3">
+                    <label for="modal-form-4" class="form-label">SEO Description</label>
+                    <textarea id="modal-form-4" class="form-control h-[215px]" name="seo_description"
+                              placeholder="SEO Description" rows="3">{{$products->seo_description}}</textarea>
+                </div>
 
-            <div class="text-right mt-5 col-span-12">
-                <a href="{{ URL::previous() }}" type="button" class="btn btn-outline-secondary w-24 mr-1">Cancel</a>
-                <button type="submit" class="btn btn-primary w-24">Save</button>
-            </div>
+                <div class="text-right mt-5 col-span-12">
+                    <a href="{{ URL::previous() }}" type="button" class="btn btn-outline-secondary w-24 mr-1">Cancel</a>
+                    <button type="submit" class="btn btn-primary w-24">Save</button>
+                </div>
 
+            </form>
         </div>
-    </form>
 @endsection
 
 @section('script')
