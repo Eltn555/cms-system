9225df030b7f73c33ea856f39902d88d0a3c6dad
Eltn555 <eltnkhan2555@gmail.com>
2024-02-18 03:39:08 +0500

    Front product: -Отзывы(design) -Характеристики -Описание

--- resources/views/livewire/front/products.blade.php
+++ resources/views/livewire/front/products.blade.php
@@ -124,112 +124,94 @@
             </div>
         </div>
     </div>
-{{--    <div class="description-review-area pb-85">--}}
-{{--        <div class="container">--}}
-{{--            <div class="description-review-topbar nav" data-aos="fade-up" data-aos-delay="200">--}}
-{{--                <a class="active" data-bs-toggle="tab" href="#des-details1"> Description </a>--}}
-{{--                <a data-bs-toggle="tab" href="#des-details2" class=""> Information </a>--}}
-{{--                <a data-bs-toggle="tab" href="#des-details3" class=""> Reviews </a>--}}
-{{--            </div>--}}
-{{--            <div class="tab-content">--}}
-{{--                <div id="des-details1" class="tab-pane active">--}}
-{{--                    <div class="product-description-content text-center">--}}
-{{--                        <p data-aos="fade-up" data-aos-delay="200">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercita ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. </p>--}}
-{{--                        <p data-aos="fade-up" data-aos-delay="400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut per unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo</p>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div id="des-details2" class="tab-pane">--}}
-{{--                    <div class="specification-wrap table-responsive">--}}
-{{--                        <table>--}}
-{{--                            <tbody>--}}
-{{--                            <tr>--}}
-{{--                                <td class="width1">Brands</td>--}}
-{{--                                <td>Airi, Brand, Draven, Skudmart, Yena</td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <td class="width1">Color</td>--}}
-{{--                                <td>Blue, Gray, Pink</td>--}}
-{{--                            </tr>--}}
-{{--                            <tr>--}}
-{{--                                <td class="width1">Size</td>--}}
-{{--                                <td>L, M, S, XL, XXL</td>--}}
-{{--                            </tr>--}}
-{{--                            </tbody>--}}
-{{--                        </table>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--                <div id="des-details3" class="tab-pane">--}}
-{{--                    <div class="review-wrapper">--}}
-{{--                        <h3>1 review for Sleeve Button Cowl Neck</h3>--}}
-{{--                        <div class="single-review">--}}
-{{--                            <div class="review-img">--}}
-{{--                                <img src="assets/images/product-details/review-1.png" alt="">--}}
-{{--                            </div>--}}
-{{--                            <div class="review-content">--}}
-{{--                                <div class="review-rating">--}}
-{{--                                    <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                    <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                    <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                    <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                    <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                </div>--}}
-{{--                                <h5><span>HasTech</span> - April 29, 2022</h5>--}}
-{{--                                <p>Donec accumsan auctor iaculis. Sed suscipit arcu ligula, at egestas magna molestie a. Proin ac ex maximus, ultrices justo eget, sodales orci. Aliquam egestas libero ac turpis pharetra, in vehicula lacus scelerisque</p>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                    <div class="ratting-form-wrapper">--}}
-{{--                        <h3>Add a Review</h3>--}}
-{{--                        <p>Your email address will not be published. Required fields are marked <span>*</span></p>--}}
-{{--                        <div class="your-rating-wrap">--}}
-{{--                            <span>Your rating</span>--}}
-{{--                            <div class="your-rating">--}}
-{{--                                <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                <a href="#"><i class="ti-star"></i></a>--}}
-{{--                                <a href="#"><i class="ti-star"></i></a>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        <div class="ratting-form">--}}
-{{--                            <form action="#">--}}
-{{--                                <div class="row">--}}
-{{--                                    <div class="col-lg-6 col-md-6">--}}
-{{--                                        <div class="rating-form-style mb-15">--}}
-{{--                                            <label>Name <span>*</span></label>--}}
-{{--                                            <input type="text">--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="col-lg-6 col-md-6">--}}
-{{--                                        <div class="rating-form-style mb-15">--}}
-{{--                                            <label>Email <span>*</span></label>--}}
-{{--                                            <input type="email">--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="col-md-12">--}}
-{{--                                        <div class="rating-form-style mb-15">--}}
-{{--                                            <label>Your review <span>*</span></label>--}}
-{{--                                            <textarea name="Your Review"></textarea>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="col-12">--}}
-{{--                                        <div class="save-email-option">--}}
-{{--                                            <p><input type="checkbox"> <label>Save my name, email, and website in this browser for the next time I comment.</label></p>--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                    <div class="col-lg-12">--}}
-{{--                                        <div class="form-submit">--}}
-{{--                                            <input type="submit" value="Submit">--}}
-{{--                                        </div>--}}
-{{--                                    </div>--}}
-{{--                                </div>--}}
-{{--                            </form>--}}
-{{--                        </div>--}}
-{{--                    </div>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-{{--        </div>--}}
-{{--    </div>--}}
+    <div class="description-review-area pb-85">
+        <div class="container">
+            <div class="description-review-topbar nav" data-aos="fade-up" data-aos-delay="200">
+                <a class="active" data-bs-toggle="tab" href="#des-details1"> Описание </a>
+                <a data-bs-toggle="tab" href="#des-details2" class=""> Характеристики </a>
+                <a data-bs-toggle="tab" href="#des-details3" class=""> Отзывы </a>
+            </div>
+            <div class="tab-content">
+                <div id="des-details1" class="tab-pane active">
+                    {!! $product->long_description !!}
+                </div>
+                <div id="des-details2" class="tab-pane">
+                    <div class="specification-wrap table-responsive d-flex justify-content-center">
+                        {!! $product->additional !!}
+                    </div>
+                </div>
+                <div id="des-details3" class="tab-pane">
+                    <div class="review-wrapper">
+                        <h3>1 review for Sleeve Button Cowl Neck</h3>
+                        <div class="single-review">
+                            <div class="review-img">
+                                <img src="assets/images/product-details/review-1.png" alt="">
+                            </div>
+                            <div class="review-content">
+                                <div class="review-rating">
+                                    <a href="#"><i class="ti-star"></i></a>
+                                    <a href="#"><i class="ti-star"></i></a>
+                                    <a href="#"><i class="ti-star"></i></a>
+                                    <a href="#"><i class="ti-star"></i></a>
+                                    <a href="#"><i class="ti-star"></i></a>
+                                </div>
+                                <h5><span>HasTech</span> - April 29, 2022</h5>
+                                <p>Donec accumsan auctor iaculis. Sed suscipit arcu ligula, at egestas magna molestie a. Proin ac ex maximus, ultrices justo eget, sodales orci. Aliquam egestas libero ac turpis pharetra, in vehicula lacus scelerisque</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="ratting-form-wrapper">
+                        <h3>Add a Review</h3>
+                        <p>Your email address will not be published. Required fields are marked <span>*</span></p>
+                        <div class="your-rating-wrap">
+                            <span>Your rating</span>
+                            <div class="your-rating">
+                                <a href="#"><i class="ti-star"></i></a>
+                                <a href="#"><i class="ti-star"></i></a>
+                                <a href="#"><i class="ti-star"></i></a>
+                                <a href="#"><i class="ti-star"></i></a>
+                                <a href="#"><i class="ti-star"></i></a>
+                            </div>
+                        </div>
+                        <div class="ratting-form">
+                            <form action="#">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="rating-form-style mb-15">
+                                            <label>Name <span>*</span></label>
+                                            <input type="text">
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-6">
+                                        <div class="rating-form-style mb-15">
+                                            <label>Email <span>*</span></label>
+                                            <input type="email">
+                                        </div>
+                                    </div>
+                                    <div class="col-md-12">
+                                        <div class="rating-form-style mb-15">
+                                            <label>Your review <span>*</span></label>
+                                            <textarea name="Your Review"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="col-12">
+                                        <div class="save-email-option">
+                                            <p><input type="checkbox"> <label>Save my name, email, and website in this browser for the next time I comment.</label></p>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-12">
+                                        <div class="form-submit">
+                                            <input type="submit" value="Submit">
+                                        </div>
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
     <div class="related-product-area pb-95">
         <div class="container">
             <div class="pt-3 mb-75 font-cormorant position-relative" data-aos="fade-up" data-aos-delay="200">
