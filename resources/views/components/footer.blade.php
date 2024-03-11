e7edfa05b8f00540930f2806776083e27f6003cc
Eltn555 <eltnkhan2555@gmail.com>
2024-03-11 10:21:35 +0500

    Footer address clickable

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -10,47 +10,40 @@
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-2 col-md-6 col-sm-6 col-12">
+                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                         <div class=" footer-widget footer-list mb-40">
-                            <h3 class="footer-title text-white font-kyiv">Information</h3>
+                            <h3 class="footer-title text-white font-kyiv">Шоурумы</h3>
                             <ul class="text-white font-kyiv">
-                                <li><a class="text-white font-kyiv" href="about-us.html">About Us</a></li>
-                                <li><a class="text-white font-kyiv" href="#">Delivery Information</a></li>
-                                <li><a class="text-white font-kyiv" href="#">Privacy Policy</a></li>
-                                <li><a class="text-white font-kyiv" href="#">Terms & Conditions</a></li>
-                                <li><a class="text-white font-kyiv" href="#">Customer Service</a></li>
+                                <li><a class="text-white font-kyiv" href="https://yandex.com/maps/-/CDFGjUId">г. Ташкент, Ц5  (Напротив Респуликанской пожарки)</a></li>
+                                <li><a class="text-white font-kyiv" href="https://yandex.com/maps/-/CDFGjYmD">Ул. Паркентская, дом 241</a></li>
+                                <li><a class="text-white font-kyiv" href="https://yandex.com/maps/-/CDFGj45G">Ул. Шота Руставели дом 136/1</a></li>
+                                <li><a class="text-white font-kyiv" href="https://yandex.com/maps/-/CDFGjJol">г. Самарканд, Микрорайон (Напротив поликлиники)</a></li>
                             </ul>
                         </div>
                     </div>
                     <div class="col-lg-2 col-md-6 col-sm-6 col-12">
-                        <div class="footer-widget footer-list mb-40">
-                            <h3 class="footer-title text-white font-kyiv">My Account</h3>
-                            <ul>
-                                <li><a class="text-white font-kyiv" href="my-account.html">My Account</a></li>
-                                <li><a class="text-white font-kyiv" href="#">Order History</a></li>
-                                <li><a class="text-white font-kyiv" href="wishlist.html">Wish List</a></li>
-                                <li><a class="text-white font-kyiv" href="#">Newsletter</a></li>
-                                <li><a class="text-white font-kyiv" href="#">Order History</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                         <div class="footer-widget footer-widget-margin-2 footer-address mb-40">
-                            <h3 class="footer-title text-white font-kyiv">Get in touch</h3>
+                            <h3 class="footer-title text-white font-kyiv">Интернет магазин</h3>
                             <ul>
-                                <li class="text-white font-kyiv"><span class="text-white font-kyiv">Address: </span>Your address goes here </li>
-                                <li class="text-white font-kyiv"><span class="text-white font-kyiv">Telephone Enquiry:</span> (012) 345 6789</li>
-                                <li class="text-white font-kyiv"><span class="text-white font-kyiv">Email: </span>demo@example.com</li>
+                                <li class="text-white font-kyiv"><a href="{{asset('/')}}" class="text-white font-kyiv">Главная</a></li>
+                                <li class="text-white font-kyiv"><a href="{{asset('/category')}}" class="text-white font-kyiv">Магазин</a></li>
+                                <li class="text-white font-kyiv"><a href="{{asset('/calculator')}}" class="text-white font-kyiv">Калькулятор</a></li>
                             </ul>
-                            <div class="open-time">
-                                <p class="text-white font-kyiv">Open : <span>8:00 AM</span> - Close : <span>18:00 PM</span></p>
-                                <p class="text-white font-kyiv">Saturday - Sunday : Close</p>
-                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-6 col-sm-6 col-12">
+                        <div class="footer-widget footer-widget-margin-2 footer-address mb-40">
+                            <h3 class="footer-title text-white font-kyiv">О Люмен Люкс</h3>
+                            <ul>
+                                <li class="text-white font-kyiv"><span class="text-white font-kyiv">О нас</span></li>
+                                <li class="text-white font-kyiv"><span class="text-white font-kyiv">Блог</span></li>
+                                <li class="text-white font-kyiv"><span class="text-white font-kyiv">Контакты</span></li>
+                            </ul>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 col-sm-6 col-12">
                         <div class="footer-widget footer-list mb-40">
-                            <h3 class="footer-title text-white font-kyiv">+998 333 850 555</h3>
+                            <h3 class="footer-title text-white font-kyiv">+998 555 005 444</h3>
                             <ul>
                                 <li class="text-white font-kyiv"><a class="text-white font-kyiv" href="my-account.html">My Account</a></li>
                             </ul>
