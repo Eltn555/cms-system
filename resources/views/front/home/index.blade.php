dbb50ac0850466d2b494abaaf28d91174224fe96
Eltn555 <eltnkhan2555@gmail.com>
2024-03-28 10:20:34 +0500

    Courusel bug

--- resources/views/front/home/index.blade.php
+++ resources/views/front/home/index.blade.php
@@ -34,58 +34,6 @@
 @endsection
 
 @section('content')
-    <script>
-        var header = $('.blurry-backgorund');
-        var $window = $(window);
-        header.addClass('mainPage-navbar');
-        $window.on('scroll', function() {
-            var scroll = $window.scrollTop();
-            if (scroll < 400) {
-                header.addClass('mainPage-navbar');
-            } else {
-                header.removeClass('mainPage-navbar');
-            }
-        });
-        let nextButton = document.getElementById('next');
-        let prevButton = document.getElementById('prev');
-        let carousel = document.querySelector('.carousel');
-        let listHTML = document.querySelector('.carousel .list');
-        let seeMoreButtons = document.querySelectorAll('.seeMore');
-        let backButton = document.getElementById('back');
-
-        nextButton.onclick = function(){
-            showSlider('next');
-        }
-        prevButton.onclick = function(){
-            showSlider('prev');
-        }
-        let unAcceppClick;
-        const showSlider = (type) => {
-            nextButton.style.pointerEvents = 'none';
-            prevButton.style.pointerEvents = 'none';
-
-            carousel.classList.remove('next', 'prev');
-            let items = document.querySelectorAll('.carousel .list .item');
-            if(type === 'next'){
-                listHTML.appendChild(items[0]);
-                carousel.classList.add('next');
-            }else{
-                listHTML.prepend(items[items.length - 1]);
-                carousel.classList.add('prev');
-            }
-            clearTimeout(unAcceppClick);
-            unAcceppClick = setTimeout(()=>{
-                nextButton.style.pointerEvents = 'auto';
-                prevButton.style.pointerEvents = 'auto';
-            }, 500)
-        }
-        seeMoreButtons.forEach((button) => {
-            button.onclick = function(){
-                carousel.classList.remove('next', 'prev');
-                carousel.classList.add('showDetail');
-            }
-        });
-    </script>
     <div class="bg-carousel mt-5" style="background-image: url('{{asset('preview.png')}}')">
         <div class="carousel">
             <div class="list">
@@ -621,6 +569,58 @@
             </div>
         </div>
     </div>
+    <script>
+        var header = $('.blurry-backgorund');
+        var $window = $(window);
+        header.addClass('mainPage-navbar');
+        $window.on('scroll', function() {
+            var scroll = $window.scrollTop();
+            if (scroll < 400) {
+                header.addClass('mainPage-navbar');
+            } else {
+                header.removeClass('mainPage-navbar');
+            }
+        });
+        let nextButton = document.getElementById('next');
+        let prevButton = document.getElementById('prev');
+        let carousel = document.querySelector('.carousel');
+        let listHTML = document.querySelector('.carousel .list');
+        let seeMoreButtons = document.querySelectorAll('.seeMore');
+        let backButton = document.getElementById('back');
+
+        nextButton.onclick = function(){
+            showSlider('next');
+        }
+        prevButton.onclick = function(){
+            showSlider('prev');
+        }
+        let unAcceppClick;
+        const showSlider = (type) => {
+            nextButton.style.pointerEvents = 'none';
+            prevButton.style.pointerEvents = 'none';
+
+            carousel.classList.remove('next', 'prev');
+            let items = document.querySelectorAll('.carousel .list .item');
+            if(type === 'next'){
+                listHTML.appendChild(items[0]);
+                carousel.classList.add('next');
+            }else{
+                listHTML.prepend(items[items.length - 1]);
+                carousel.classList.add('prev');
+            }
+            clearTimeout(unAcceppClick);
+            unAcceppClick = setTimeout(()=>{
+                nextButton.style.pointerEvents = 'auto';
+                prevButton.style.pointerEvents = 'auto';
+            }, 500)
+        }
+        seeMoreButtons.forEach((button) => {
+            button.onclick = function(){
+                carousel.classList.remove('next', 'prev');
+                carousel.classList.add('showDetail');
+            }
+        });
+    </script>
 @endsection
 
 @section('scripts')
