6359a68879ada0c395af232fac13088f40d3072a
Eltn555 <eltnkhan2555@gmail.com>
2024-04-07 17:58:47 +0500

    Cart menu added

---
+++ app/Http/Livewire/Front/Cart/CartItems.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Livewire\Front\Cart;
+
+use App\Models\CartProduct;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Livewire\Component;
+use App\Models\Product;
+
+class CartItems extends Component
+{
+    protected $listeners = ['checkItems'];
+    public $items;
+
+    public function checkItems(){
+        if (Auth::check()) {
+            // User is authenticated, fetch products from the database
+            return CartProduct::where('user_id', auth()->user()->id)->with('product')->get()->pluck('product');
+        } else {
+            $cartCookie = Cookie::get('cart');
+            if ($cartCookie) {
+                // User is not authenticated, fetch products from the cookie
+                $cartArray = json_decode($cartCookie, true);
+                $productIds = [];
+                foreach ($cartArray as $item) {
+                    $product = Product::where('id', $item['product_id'])->first();
+                    if (isset($item['product_id']) && $product != null) {
+                        array_push($productIds, $product);
+                    }
+                }
+                // Fetch products from the database based on the product IDs
+                return $productIds;
+            }
+        }
+        return [];
+    }
+
+    public function render()
+    {
+        $this->items = $this->checkItems();
+        return view('livewire.front.cart.cart-items');
+    }
+}
---
+++ resources/views/livewire/front/cart/cart-items.blade.php
@@ -0,0 +1,42 @@
+<div class="sidebar-cart-active">
+    <div class="sidebar-cart-all">
+        <a class="cart-close"><i class="pe-7s-close"></i></a>
+        <h1 class="font-cormorant fw-bolder pb-4 border-bottom border-1 mb-5">Корзина</h1>
+        <div class="cart-content">
+            <ul>
+                @foreach($items as $item)
+                    <li>
+                        <div class="cart-img">
+                            <a href="#"><img src="" alt=""></a>
+                        </div>
+                        <div class="cart-title">
+                            <h4><a href="#">{{$item->title}}</a></h4>
+                            <span>ID {{$item->id}}</span>
+                            <span>Count {{}}</span>
+                        </div>
+                        <div class="cart-delete">
+                            <a href="#">×</a>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
+        <div class="cart-bottom">
+            <div class="border-bottom border-1 text-secondary d-flex justify-content-between">
+                <p class="font-kyiv fs-5">Промежуточный итог:</p>
+                <p class="font-kyiv fs-5 fw-bolder text-black">1420000</p>
+            </div>
+            <div class="pt-2">
+                <p class="text-secondary font-kyiv">Налоги, стоимость доставки рассчитывается при оформлении заказа.</p>
+            </div>
+            <div class="d-flex">
+                <div class="single-product-cart btn-hover w-50 pe-1 text-center">
+                    <a href="#" class="w-100 text-dark bg-light border border-1">Посмотреть корзину</a>
+                </div>
+                <div class="single-product-cart btn-hover w-50 ps-1 text-center">
+                    <a href="#" class="w-100 text-dark">Перейти к оплате</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
