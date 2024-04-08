b6e66178cd4bd9d42291ed56221c17e27775f007
Abu-Hurayra <aaabrorov@gmail.comn>
2024-04-08 13:46:17 +0500

    Form in process

--- resources/views/livewire/profile-login-register.blade.php
+++ resources/views/livewire/profile-login-register.blade.php
@@ -1,10 +1,10 @@
     <div class="login-register-wrapper mx-5 mb-5" style="margin-top: 150px">
         <div class="login-register-tab-list nav" role="tablist">
             <a class="active" data-bs-toggle="tab" href="#lg1" aria-selected="true" role="tab">
-                <h4> login </h4>
+                <h4> Вход </h4>
             </a>
             <a data-bs-toggle="tab" href="#lg2" aria-selected="false" tabindex="-1" role="tab">
-                <h4> register </h4>
+                <h4> Регистрация </h4>
             </a>
         </div>
         <div class="tab-content">
@@ -13,15 +13,15 @@
                     <div class="login-register-form">
                         <form action="{{ route('login') }}" method="POST">
                             @csrf
-                            <input type="text" name="email" placeholder="Username">
-                            <input type="password" name="password" placeholder="Password">
+                            <input type="text" name="email" placeholder="Имя">
+                            <input type="password" name="password" placeholder="Пароль">
                             <div class="login-toggle-btn">
                                 <input type="checkbox">
-                                <label>Remember me</label>
-                                <a href="#">Forgot Password?</a>
+                                <label>Запомнить меня</label>
+                                <a href="#">Забыли пароль?</a>
                             </div>
                             <div class="button-box btn-hover">
-                                <button type="submit">Login</button>
+                                <button type="submit">Войти</button>
                             </div>
                         </form>
                     </div>
@@ -32,12 +32,12 @@
                     <div class="login-register-form">
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
-                            <input type="text" name="name" placeholder="Username">
-                            <input name="email" placeholder="Email" type="email">
-                            <input type="password" name="password" placeholder="Password">
-                            <input type="password" name="password_confirmation" placeholder="Confirm password" required autocomplete="new-password">
+                            <input type="text" name="name" placeholder="Имя">
+                            <input name="email" placeholder="Email" type="Почта">
+                            <input type="password" name="password" placeholder="Пароль">
+                            <input type="password" name="password_confirmation" placeholder="Подтверждение пароля" required autocomplete="new-password">
                             <div class="button-box btn-hover">
-                                <button type="submit">Register</button>
+                                <button type="submit">Регистрация</button>
                             </div>
                         </form>
                     </div>
