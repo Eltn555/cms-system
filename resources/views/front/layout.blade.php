34da0c905bb812ba95ac816c4f2deb520a541fb3
Eltn555 <eltnkhan2555@gmail.com>
2024-04-09 13:07:35 +0500

    Script

--- resources/views/front/layout.blade.php
+++ resources/views/front/layout.blade.php
@@ -43,6 +43,8 @@
     @stack('styles')
     @yield('style')
     @livewireScripts
+    <script src='https://salebot.pro/js/salebot.js?v=1' charset='utf-8'></script>
+    <script>  SaleBotPro.init({    onlineChatId: '2893'  });</script>
 </head>
 
 <body>
