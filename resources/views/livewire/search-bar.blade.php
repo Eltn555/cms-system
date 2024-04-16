bc805e4139f22b263fe486ad84ecec16a4ca1c5c
Abu-Hurayra <aaabrorov@gmail.comn>
2024-04-16 16:14:18 +0500

    Form in process

--- resources/views/livewire/search-bar.blade.php
+++ resources/views/livewire/search-bar.blade.php
@@ -1,4 +1,4 @@
-<div class="me-2 header-action-style header-search-1">
+<div class=" header-action-style header-search-1">
     <a class="search-toggle {{($search == "") ? "" : "open"}}" href="#">
         <svg class="s-open white-icon d-none" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
             <circle cx="11.5" cy="11.5" r="9.5" stroke="white" stroke-width="1.5"/>
