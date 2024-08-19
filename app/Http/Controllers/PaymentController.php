6e44d002c684e6f90c7d8465b7bcf699dbe50fe4
Eltn555 <eltnkhan2555@gmail.com>
2024-08-20 00:53:48 +0500

    Payme

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -101,7 +101,7 @@ class PaymentController extends Controller
                     ]
                 ]);
             } else {
-                return response()->json(['result' => ['code' => -31060, 'message' => 'Transaction already paid']], 200);
+                return response()->json(['result' => ['code' => -31060, 'message' => 'Transaction already paid']], 409);
             }
         }else{
             return response()->json(['result' => ['allow' => -31003, 'message' => 'Transaction not found']], 404);
