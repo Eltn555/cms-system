e64e1384dffe12fb0b389e8194553bbd1c620577
Eltn555 <eltnkhan2555@gmail.com>
2024-10-05 02:18:29 +0500

    Delivery info

--- app/Http/Livewire/Front/Products.php
+++ app/Http/Livewire/Front/Products.php
@@ -15,8 +15,14 @@ class Products extends Component
 
     public function mount($slug){
         $this->product = Product::with('images', 'tags', 'additional_tags', 'categories')->where('slug', $slug)->firstOrFail();
-        $relatedTags = $this->product->tags()->with('products')->inRandomOrder()->take(10)->get();
-        $additionalTags = $this->product->additional_tags()->with('products')->inRandomOrder()->take(10)->get();
+
+        $relatedTags = $this->product->tags()->with(['products' => function($query) {
+            $query->where('status', 1);
+        }])->inRandomOrder()->take(10)->get();
+
+        $additionalTags = $this->product->additional_tags()->with(['products' => function($query) {
+            $query->where('status', 1);
+        }])->inRandomOrder()->take(10)->get();
 
         $this->relatedProducts = $relatedTags->pluck('products')->flatten()->shuffle()->unique('id')->take(10);
         $this->additionalProducts = $additionalTags->pluck('products')->flatten()->shuffle()->unique('id')->take(10);
