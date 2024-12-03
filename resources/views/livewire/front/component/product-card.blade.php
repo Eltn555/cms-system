d7a49276f616b198842ed098235224d6c273e800
Eltn555 <eltnkhan2555@gmail.com>
2024-12-03 12:56:55 +0500

    Admin roles

--- resources/views/livewire/front/component/product-card.blade.php
+++ resources/views/livewire/front/component/product-card.blade.php
@@ -3,12 +3,19 @@
                 <a href="{{route('front.product.show', ['slug' => $product->slug])}}">
                     <img class="image{{$product->id}}" src="{{asset(($image) ? 'storage/'.$image->image : 'no_photo.jpg')}}" alt="{{$product->title}}" loading="lazy">
                 </a>
-{{--                <div--}}
-{{--                    class="product-badge flex justify-end badge-top badge-right badge-pink">--}}
-{{--                        @foreach($tags as $tag)--}}
-{{--                            <div class="p-1 mb-1 rounded-1 text-white fw-semibold tag-view">{{ $tag->visible === 1 ? $tag->title : '' }}</div>--}}
-{{--                        @endforeach--}}
-{{--                </div>--}}
+                <div
+                    class="product-badge rounded-0 flex justify-end badge-pink" style="top: 5px; left: 5px;">
+                        @foreach($tags as $tag)
+                            <div class="px-2 mb-1 d-flex justify-content-center align-items-center rounded-0 text-white fw-semibold tag-view">{{ $tag->visible == 1 ? $tag->title : '' }}</div>
+                        @endforeach
+                </div>
+                @if($product->amount > 0)
+                    <div class="position-absolute bottom-0 end-0">
+                        <div class="px-2 mb-1 me-1 rounded-1 text-success text-uppercase" style="background-color: rgba(180,180,180,0.45)">
+                            <p class="mb-0 fw-bolder">В наличии</p>
+                        </div>
+                    </div>
+                @endif
                 <div class="product-action-wrap">
                     @livewire('front.wishlist.wishlist-button', ['product' => $product], key($product->id))
 {{--                    <button class="product-action-btn-1" title="Quick View"--}}
