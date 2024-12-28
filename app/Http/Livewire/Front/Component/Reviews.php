3c61319b08cc8f3fccb5708dd59dcc8f7cd1ef5b
Eltn555 <eltnkhan2555@gmail.com>
2024-12-29 01:52:38 +0500

    Review front functional

--- app/Http/Livewire/Front/Component/Reviews.php
+++ app/Http/Livewire/Front/Component/Reviews.php
@@ -3,20 +3,75 @@
 namespace App\Http\Livewire\Front\Component;
 
 use Livewire\Component;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Product;
 use App\Models\Review;
 
 class Reviews extends Component
 {
     public $product;
-    public $rated = 0;
+    public $text;
+    public $rated;
+    public $reviews;
 
     public function mount($product){
         $this->product = $product;
+        $this->reviews = Review::where('product_id', $this->product->id)->get();
+
+//        dd($this->reviews);
+        if (Auth::user()){
+            $review = Review::where('user_id', auth()->id())
+                ->where('product_id', $this->product->id)
+                ->first();
+
+            if ($review) {
+                $this->rated = $review->rate;
+                $this->text = $review->text ?? ''; // Assuming the `text` column exists in the Review model
+            }
+        }
     }
 
     public function rate($point){
         $this->rated = $point;
+        if (Auth::user()){
+            $userId = auth()->id();
+            // Create or update the review
+            Review::updateOrCreate(
+                [
+                    'user_id' => $userId,
+                    'product_id' => $this->product->id,
+                ],
+                [
+                    'rate' => $this->rated,
+                ]
+            );
+            session()->flash('message', 'Rating submitted successfully!');
+        }else{
+            session()->flash('message', 'Please register!');
+        }
+    }
+
+    public function submit(){
+        if ($this->rated != 0){
+            if (Auth::user()){
+                $userId = auth()->id();
+                // Create or update the review
+                Review::updateOrCreate(
+                    [
+                        'user_id' => $userId,
+                        'product_id' => $this->product->id,
+                    ],
+                    [
+                        'text' => $this->text,
+                    ]
+                );
+                session()->flash('message', 'Rating submitted successfully!');
+            }else{
+                session()->flash('message', 'Please register!');
+            }
+        }else{
+            session()->flash('message', 'Please rate!');
+        }
     }
 
     public function render()
--- app/Http/Livewire/Front/Products.php
+++ app/Http/Livewire/Front/Products.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Front;
 use App\Models\Product;
 use Livewire\Component;
 use App\Models\Tag;
+use App\Models\Review;
 
 class Products extends Component
 {
@@ -15,10 +16,13 @@ class Products extends Component
     public $viewed;
     public $profit;
     public $profPercent;
+    public $rate;
+    public $rates;
 
     public function mount($slug){
         $this->product = Product::with('images', 'tags', 'additional_tags', 'categories')->where('slug', $slug)->firstOrFail();
-
+        $this->rate = Review::where('product_id', $this->product->id)->avg('rate');
+        $this->rates = Review::where('product_id', $this->product->id)->count('rate');
         $this->viewed = $this->product->image ?? 0;
         $this->viewed += 1;
         $this->product->update(['image' => $this->viewed]);
