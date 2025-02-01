e4772609dab21038d06ccb9ebc6922d1082256f6
Eltn555 <eltnkhan2555@gmail.com>
2025-02-01 15:09:14 +0500

    Portfolio Structure

---
+++ database/migrations/2025_02_01_135651_create_portfolios_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('portfolios', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('image')->nullable(); // Cover image
+            $table->text('text')->nullable();
+            $table->string('video')->nullable(); // Video URL or file path
+            $table->text('info')->nullable(); // Additional info
+            $table->foreignId('category_id')->constrained('portfolio_categories')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('portfolios');
+    }
+};
