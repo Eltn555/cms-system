6c63cbe28f7d7ddfe13be6712f867b5e0980325c
Eltn555 <eltnkhan2555@gmail.com>
2025-02-10 14:34:09 +0500

    Bug

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -68,10 +68,6 @@
             box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1); /* Medium shadow */
         }
 
-        .flex {
-            display: flex;
-        }
-
         .items-center {
             align-items: center;
         }
