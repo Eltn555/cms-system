d87bd07517d7f5b811c3f313cd64636b02829e2f
Eltn555 <eltnkhan2555@gmail.com>
2025-02-19 00:32:20 +0500

    Test

--- app/Http/Middleware/BasicAuthMiddleware.php
+++ app/Http/Middleware/BasicAuthMiddleware.php
@@ -20,7 +20,7 @@ class BasicAuthMiddleware
         [$username, $password] = explode(':', $decodedCredentials, 2);
         $expectedUsername = env('UZUM_USERNAME');
         $expectedPassword = env('UZUM_PASSWORD');
-        return response()->json(['error' => 'Unauthorized'.$username.$password.$expectedUsername.$expectedPassword], 401);
+        return response()->json(['error' => 'Unauthorized'], 401);
 
         if ($username != $expectedUsername || $password != $expectedPassword) {
             return response()->json(['error' => 'Unauthorized'], 401);
