b82f1ca3c886d2c688ecd5919cbd127833dc31c7
Eltn555 <eltnkhan2555@gmail.com>
2025-04-08 16:10:56 +0500

    Fix SaleBot

--- app/Http/Controllers/SaleBotController.php
+++ app/Http/Controllers/SaleBotController.php
@@ -10,16 +10,8 @@ class SaleBotController extends Controller
     {
         // URL of the external script
         $scriptUrl = 'https://salebot.pro/js/salebot.js?v=1';
-
-        // Fetch the script content
         $scriptContent = file_get_contents($scriptUrl);
 
-        // Check if script was fetched successfully
-        if ($scriptContent === false) {
-            abort(500, 'Unable to fetch the SaleBot script.');
-        }
-
-        // Set the appropriate headers
         return response($scriptContent, 200)
             ->header('Content-Type', 'application/javascript')
             ->header('Cross-Origin-Resource-Policy', 'same-origin');
