1218f0a61658e4f5c256b063f80dc8ba17c5c34b
Eldor <eldorsobitov18@gmail.com>
2025-07-19 00:26:52 +0500

    Enhance Led calculator functionality and UI - Add product loading based on selected categories - Implement pagination for product display - Update UI for room type selection and results display - Introduce error handling for category selection

--- app/Http/Livewire/Front/Calculator/Led.php
+++ app/Http/Livewire/Front/Calculator/Led.php
@@ -5,6 +5,7 @@ namespace App\Http\Livewire\Front\Calculator;
 use Livewire\Component;
 use App\Models\Setting;
 use App\Models\Category;
+use App\Models\Product;
 
 class Led extends Component
 {   
@@ -12,14 +13,64 @@ class Led extends Component
     public $ledRoomTypes;
     public $ledRoomType;
     public $ledCategory;
+    public $allProducts;
     public $ledAccessoriesCategory;
     public $ledPowerBlocksCategory;
     public $ledMeter = '';
     public $error = '';
+    public $pagesize = 24;
+    public $watt = 0;
+    public $products;
+    public $allPowerBlocks;
+    public $allAccessories;
 
     public function mount()
     {
-        $this->res();
+        $this->calculator = Setting::getByGroup('calculator');
+
+        if($this->calculator){
+            $this->ledRoomTypes = $this->calculator->where('setting_key', 'led_rooms')->values() ?? collect();
+            $this->ledCategory = $this->calculator->where('setting_key', 'led_category')->values()->first() ?? collect();
+            $this->ledAccessoriesCategory = $this->calculator->where('setting_key', 'led_accessory_category')->values()->first() ?? collect();
+            $this->ledPowerBlocksCategory = $this->calculator->where('setting_key', 'power_block_category')->values()->first() ?? collect();
+        }
+
+        $category = Category::find($this->ledCategory->setting_value);
+
+        if($category){
+            $this->allProducts = $category->products()->
+            select('products.id', 'title', 'slug', 'price', 'discount_price', 'amount', 'watt', 'kelvin')->
+            with('images', 'tags')->
+            where('status', 1)->get();
+        } else {
+            $this->allProducts = collect();
+        }
+
+        $powerBlockCategory = Category::find($this->ledPowerBlocksCategory->setting_value);
+
+        if($powerBlockCategory){
+            $this->allPowerBlocks = $powerBlockCategory->products()->
+            select('products.id', 'title', 'slug', 'price', 'discount_price', 'amount', 'watt', 'kelvin')->
+            with('images', 'tags')->
+            where('status', 1)->take(24)->get();
+        } else {
+            $this->allPowerBlocks = collect();
+        }
+
+        $accessoriesCategory = Category::find($this->ledAccessoriesCategory->setting_value);
+
+        if($accessoriesCategory){
+            $this->allAccessories = $accessoriesCategory->products()->
+            select('products.id', 'title', 'slug', 'price', 'discount_price', 'amount', 'watt', 'kelvin')->
+            with('images', 'tags')->
+            where('status', 1)->take(24)->get();
+        } else {
+            $this->allAccessories = collect();
+        }
+    }
+
+    public function setLedRoomType($value){
+        $this->ledRoomType = $this->ledRoomType == $value ? null : $value;
     }
 
     public function calculate(){
@@ -36,34 +87,36 @@ class Led extends Component
             return;
         }
 
-        // Add default category if no specific selections are made
-        if (!$this->ledCategory) {
-            $categories[] = $this->ledCategory->setting_value;
-        } else{
-            $this->error = '* Категория не выбрана в админ панели';
-            return;
-        }
-        
-        if($this->ledMeter > 0){
-            $this->emit('setProducts', $this->ledMeter, $categories);
+        $category = Category::find($this->ledCategory->setting_value);
+
+        if($category){
+            $this->allProducts = $category->products()->
+            select('products.id', 'title', 'slug', 'price', 'discount_price', 'amount', 'watt', 'kelvin')->
+            with('images', 'tags')->
+            where('status', 1)->
+            where('kelvin', '>', $this->ledRoomType - 500)->
+            where('kelvin', '<', $this->ledRoomType + 500)->
+            get();
+
+            $this->products = $this->allProducts;
         } else {
-            $this->emit('setProducts', null, $categories);
+            $this->allProducts = collect();
         }
     }
 
-    public function res(){
-        $this->calculator = Setting::getByGroup('calculator');
-
-        if($this->calculator){
-            $this->ledRoomTypes = $this->calculator->where('setting_key', 'led_rooms')->values() ?? collect();
-            $this->ledCategory = $this->calculator->where('setting_key', 'led_category')->values()->first() ?? collect();
-            $this->ledAccessoriesCategory = $this->calculator->where('setting_key', 'led_accessory_category')->values()->first() ?? collect();
-            $this->ledPowerBlocksCategory = $this->calculator->where('setting_key', 'led_power_block_category')->values()->first() ?? collect();
-        }
+    public function loadNext(){
+        $this->pagesize += 24;
     }
 
     public function render()
     {
-        return view('livewire.front.calculator.led');
+        $this->products = $this->allProducts->take($this->pagesize);
+
+        return view('livewire.front.calculator.led', [
+            'convertedProducts' => $this->products,
+            'kelvin' => $this->ledRoomType,
+            'watt' => $this->watt,
+            'showMore' => $this->pagesize < $this->allProducts->count()
+        ]);
     }
 }
--- resources/views/livewire/front/calculator/led.blade.php
+++ resources/views/livewire/front/calculator/led.blade.php
@@ -28,7 +28,7 @@
         }
         .room-type label:hover, .room-type label.active{
             background-color: #ffffff;
-            box-shadow: inset 0 0 5px 1px #8ab2ff !important;
+            box-shadow: 0 0 10px 3px #8ab2ff !important;
             border: 2px solid #8ab2ff;
         }
 
@@ -157,11 +157,11 @@
 <div class="container pb-5 px-0" style="max-width: 1400px; margin: 0 auto;">
     <div class="row w-100 m-0 px-1 px-md-2 px-lg-3">
         <!-- Room type -->
-        <div class="col-12 mb-3 room-type px-0 d-flex flex-wrap align-self-start" wire:ignore>
+        <div class="col-12 mb-3 room-type px-0 d-flex flex-wrap align-self-start">
             <h3 class="font-kyiv fs-5 fw-bold w-100">Выберите тип помещения</h3>
             @foreach ($ledRoomTypes as $roomType)
-                <div style="padding: 0.1rem;" class="col-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2">
-                    <label for="lux{{ $roomType->setting_value }}" class="rounded-1 w-100 h-100 shadow-sm d-flex align-items-center justify-content-between flex-column position-relative">
+                <div style="padding: 0.1rem;" class="col-6 col-md-4 col-lg-3 col-xl-2 col-xxl-2 ">
+                    <label onclick="setLoading(true);" for="lux{{ $roomType->setting_value }}" class="rounded-1 w-100 h-100 shadow-sm d-flex align-items-center justify-content-between flex-column position-relative {{ $roomType->setting_value == $ledRoomType ? 'active' : '' }}">
                         <div class="p-2 text-start position-absolute top-0 start-0 info-box">
                             <h3 class="font-kyiv fs-6 fw-bold mb-0 text-white">{{ $roomType->title }}</h3>
                             <p class="font-kyiv fw-bolder mb-0 text-white">{{ $roomType->description }}</p>
@@ -169,7 +169,7 @@
                         <div class="room-icon w-100">
                             <img src="{{ asset('storage/'.$roomType->media) }}" alt="{{ $roomType->title }}" class="w-100 h-100 rounded-1 object-fit-cover">
                         </div>
-                        <input wire:model="ledRoomType" name="led-room-type" id="lux{{ $roomType->setting_value }}" type="radio" value="{{ $roomType->setting_value }}">
+                        <input wire:click="setLedRoomType({{ $roomType->setting_value }})" name="led-room-type" id="lux{{ $roomType->setting_value }}" type="radio" value="{{ $roomType->setting_value }}">
                     </label>
                 </div>
             @endforeach
@@ -186,6 +186,50 @@
                     </div>
                 </div>
             </div>
+        </div>
+
+        <!-- Calculate -->
+        <div class="p-1 col-12 my-3 d-flex flex-column justify-content-center">
+            <button onclick="setLoading(true);" wire:click="calculate" class="calc-more-btn w-100">Рассчитать <i class="ps-2 fa fa-calculator"></i></button>
+            <p class="text-danger fs-6 mt-2 font-kyiv fw-bold m-0 p-0 col-12">{{ $error }}</p>
+        </div>
+        <!-- Results -->
+        <div class="row w-100 m-0 px-1 px-md-2 px-lg-3">
+            <div class="col-12 my-3 d-flex justify-content-between gap-2 mt-3">
+                <h3 class="font-kyiv fs-3 fw-bold">Результаты</h3>
+            </div>
+            @foreach ($convertedProducts as $product)
+                <div class="col-6 p-2 col-sm-4 col-md-3 col-lg-3 col-xl-2" wire:key="product-{{ $product->id ?? $product['id'] }}-{{ $kelvin }}">
+                    <livewire:front.component.product-calc :product="$product" :lux="$kelvin" :wire:key="'calc-'.$product->id.'-'.$kelvin"/>
+                </div>
+            @endforeach
+            @if($showMore)
+                <div class="d-flex gap-2 justify-content-center w-100 mt-3">
+                    <button onclick="setLoading(true);" wire:click="loadNext" class="calc-more-btn">Показать ещё <i class="fa fa-angles-right"></i></button>
+                </div>
+            @endif
+        </div>
+        <!-- Power block -->
+        <div class="row w-100 m-0 px-1 px-md-2 px-lg-3">
+            <div class="col-12 my-3 d-flex justify-content-between gap-2 mt-3">
+                <h3 class="font-kyiv fs-3 fw-bold">Блок питания</h3>
+            </div>
+            @foreach ($allPowerBlocks as $product)
+                <div class="col-6 p-2 col-sm-4 col-md-3 col-lg-3 col-xl-2" wire:key="product-{{ $product->id ?? $product['id'] }}-{{ $kelvin }}">
+                    <livewire:front.component.product-calc :product="$product" :lux="$kelvin" :wire:key="'calc-'.$product->id.'-'.$kelvin"/>
+                </div>
+            @endforeach
+        </div>
+        <!-- Accessories -->
+        <div class="row w-100 m-0 px-1 px-md-2 px-lg-3">
+            <div class="col-12 my-3 d-flex justify-content-between gap-2 mt-3">
+                <h3 class="font-kyiv fs-3 fw-bold">Аксессуары</h3>
+            </div>
+            @foreach ($allAccessories as $product)
+                <div class="col-6 p-2 col-sm-4 col-md-3 col-lg-3 col-xl-2" wire:key="product-{{ $product->id ?? $product['id'] }}-{{ $kelvin }}">
+                    <livewire:front.component.product-calc :product="$product" :lux="$kelvin" :wire:key="'calc-'.$product->id.'-'.$kelvin"/>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
