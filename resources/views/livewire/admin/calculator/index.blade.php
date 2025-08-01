0765af501c4f8d0f3bbb82491165044085aa7071
Eldor <eldorsobitov18@gmail.com>
2025-08-01 17:12:00 +0500

    Fix update button

--- resources/views/livewire/admin/calculator/index.blade.php
+++ resources/views/livewire/admin/calculator/index.blade.php
@@ -401,7 +401,7 @@
                     @error('currentReelType.media') <p class="text-sm text-danger">{{ $message }}</p> @enderror
                 </div>
                 <button wire:click.prevent="createNew('currentReelType')" type="button" class="{{$updating['var']=='currentReelType' ? 'hidden' : ''}} btn btn-success text-white" style="align-self: flex-start;">Добавить</button>
-                <button wire:click.prevent="update('currentLedRoom')" type="button" class="{{$updating['var']!='currentLedRoom' ? 'hidden' : ''}} btn btn-primary text-white" style="align-self: flex-start;">Обновить</button>
+                <button wire:click.prevent="update('currentReelType')" type="button" class="{{$updating['var']!='currentReelType' ? 'hidden' : ''}} btn btn-primary text-white" style="align-self: flex-start;">Обновить</button>
                 <button wire:click.prevent="edit({{ $updating['id'] }}, 'currentReelType')" type="button" class="btn btn-danger text-white {{ $updating['var']!='currentReelType' ? 'hidden' : '' }}" style="align-self: flex-start;">
                     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-x-icon lucide-x"><path d="M18 6 6 18"/><path d="m6 6 12 12"/></svg>
                 </button>
